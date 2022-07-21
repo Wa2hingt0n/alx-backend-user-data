44d558674624747d84fd2a9ef2a31b489b1d2526
Washington Obungu <tobungu@gmail.com>
2022-07-21 15:21:35 +0000

    Implemented log record formatting

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ Defines a function 'filter_datum' """
+import logging
 import re
 from typing import List
 
@@ -22,3 +23,24 @@ def filter_datum(
         regex = '(?<=' + field + '=).*?(?=;)'
         message = re.sub(regex, redaction, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Formats the log record """
+        formatted_record = super(RedactingFormatter, self).format(record)
+        logged_record = filter_datum(self.fields, self.REDACTION,
+                                     formatted_record,
+                                     self.SEPARATOR)
+        return logged_record
