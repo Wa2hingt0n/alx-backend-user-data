f09c425ce208755a3160e8a1a574768147875ca7
Washington Obungu <tobungu@gmail.com>
2022-08-04 14:24:04 +0000

    Fixed typo on shebang

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env pyhton3
+#!/usr/bin/env python3
 """ Creates an SQLAlchemy model named 'User' """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
