d9c5d4a1b1e3dc1c587f6caa791b327b0b635b8a
Washington Obungu <tobungu@gmail.com>
2022-08-05 21:57:00 +0000

    Added update_password and get_reset_password_token methods

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -76,3 +76,29 @@ class Auth:
         if user_id is None:
             return None
         self._db.update_user(user_id, session_id=None)
+
+    def reset_password_token(self, email: str) -> str:
+        """ Generates a reset password token """
+        user = None
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            user = None
+        if user is None:
+            raise ValueError()
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Find's user by  reset token and updates the hashed_password """
+        user = None
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            user = None
+        if user is None:
+            raise ValueError()
+        new_password = _hash_password(password)
+        self._db.update_user(
+            user.id, hashed_password=new_password_hash, reset_token=None)
