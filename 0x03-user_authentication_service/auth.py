4d1a71d2d17cd17e7b500c80f2b0a6747172569e
Washington Obungu <tobungu@gmail.com>
2022-08-05 22:03:46 +0000

    Renamed method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -88,7 +88,7 @@ class Auth:
             raise ValueError()
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
-        return reset token
+        return reset_token
 
     def update_password(self, reset_token: str, password: str) -> None:
         """Find's user by  reset token and updates the hashed_password """
