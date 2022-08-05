e5121e44a5549ccf44cd38c1a8a2867f0a603c62
Washington Obungu <tobungu@gmail.com>
2022-08-05 17:03:44 +0000

    Implemented /users endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python3
 """ Creates a flask app """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", strict_slashes=False)
-def index():
+def index() -> str:
     """ Returns a payload message """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", strict_slashes=False, methods=["POST"])
+def users() -> str:
+    """ User registration endpoint """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
