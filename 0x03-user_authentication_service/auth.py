92bf2b9cbbc2b7ebeb402cdf7049dcc64b4ab064
Washington Obungu <tobungu@gmail.com>
2022-08-05 20:55:35 +0000

    Implemented destroy_session method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -70,3 +70,9 @@ class Auth:
             except NoResultFound:
                 return None
         return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """ Updates an existing session_id to None """
+        if user_id is None:
+            return None
+        self._db.update_user(user_id, session_id=None)
